d2268ae3adbfaed8e2000c7e835f2812bb463fa7
Gonçalo Figueiredo <goncalofigueira53@gmail.com>
2020-03-02 17:54:26 +0000

    Adicionar arquivos de projeto.

---
+++ TopScore/Contador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopScore
+{
+    class Contador
+    {
+        private int _pontos;
+        
+        public string Score()
+        {
+            _pontos += 1; 
+        }
+    }
+}
